f62c1ad26344b3e54c8740c2c0916c2b4b70abca
oscharko <o.scharkowski@oscharko.de>
2022-07-16 19:49:33 +0200

    Create new Spring-Module Pet-Clinic-Web closes #3

---
+++ Spring-Pet-Clinic-Monolith-in-Kotlin/Pet-Clinic-Web/src/test/kotlin/de/oscharko/petclinicweb/PetClinicWebApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.oscharko.petclinicweb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PetClinicWebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
