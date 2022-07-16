940cffa9c601bd4e4a3ca91f78fba6d5c5e4b542
oscharko <o.scharkowski@oscharko.de>
2022-07-16 19:32:08 +0200

    Create new Spring project in Kotlin closes #1

---
+++ Spring-Pet-Clinic-Monolith-in-Kotlin/src/main/kotlin/de/oscharko/springpetclinicmonolithinkotlin/SpringPetClinicMonolithInKotlinApplication.kt
@@ -0,0 +1,11 @@
+package de.oscharko.springpetclinicmonolithinkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringPetClinicMonolithInKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringPetClinicMonolithInKotlinApplication>(*args)
+}
---
+++ Spring-Pet-Clinic-Monolith-in-Kotlin/src/test/kotlin/de/oscharko/springpetclinicmonolithinkotlin/SpringPetClinicMonolithInKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.oscharko.springpetclinicmonolithinkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringPetClinicMonolithInKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
